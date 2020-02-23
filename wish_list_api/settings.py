58dd4ea5c9f9c8f530a95ab4940d7851960ebbf0
Ehren Davis <ehren.davis12@gmail.com>
2020-02-22 21:49:42 -0700

    clean up

--- wish_list_api/settings.py
+++ wish_list_api/settings.py
@@ -86,18 +86,6 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # },
-    # 'default': {
-    #     # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     # Or path to database file if using sqlite3.
-    #     'NAME': os.environ.get('Database', 'wish-list'),
-    #     'USER': os.environ.get('USER', 'postgres'),                      # Not used with sqlite3.
-    #     'PASSWORD': os.environ.get('PASSWORD', 'postgres'),                  # Not used with sqlite3.
-    #     # Set to empty string for localhost. Not used with sqlite3.
-    #     'HOST': os.environ.get('HOST', 'localhost'),
-    #     # Set to empty string for default. Not used with sqlite3.
-    #     'PORT': int(os.environ.get('PORT', 5432)),
-    # }
     'default': {
         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -111,11 +99,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-# DATABASES = {}
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
-print(DATABASES['default'])
 
 
 # Password validation
