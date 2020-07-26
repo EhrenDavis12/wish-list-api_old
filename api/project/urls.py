b3f0301814eb9f2a254cd6dd6362d9bc3b805670
Ehren davis <41214508+EhrenDavis12@users.noreply.github.com>
2020-07-26 16:02:43 -0700

    Lets go with this auth for now (#12)
    * update gunicorn to point to our project
    
    * lets go with this for now it works for what we need and we can now move on.

--- api/project/urls.py
+++ api/project/urls.py
@@ -1,18 +1,21 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import (ItemViewSet, ItemLinkViewSet)
-from rest_framework import renderers
+from .views import ItemViewSet, ItemLinkViewSet
+from .views import FriendViewSet, BelongingViewSet, BorrowedViewSet
 
+# from rest_framework import renderers
 
 
 router = routers.DefaultRouter()
 # router.trailing_slash = "/?"
 
-router.register(r'item', ItemViewSet,
-                basename='Item')
-router.register(r'item_link', ItemLinkViewSet,
-                basename='Itemlink')
+router.register(r"item", ItemViewSet, basename="Item")
+router.register(r"item_link", ItemLinkViewSet, basename="Itemlink")
+
+router.register(r"friends", FriendViewSet)
+router.register(r"belongings", BelongingViewSet)
+router.register(r"borrowings", BorrowedViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
--- api/project/views.py
+++ api/project/views.py
@@ -1,30 +1,34 @@
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from .models import Item, ItemLink
-from rest_framework import viewsets, status
+from .models import Friend, Belonging, Borrowed
+from rest_framework import viewsets
 from .serializers import ItemSerializer, ItemLinkSerializer
+from .serializers import FriendSerializer, BelongingSerializer, BorrowedSerializer
 from rest_framework import permissions
-from .permissions import IsOwnerOrReadOnly
+from .permissions import IsOwnerOrReadOnly, IsOwner
 from rest_framework.response import Response
+
 # Create your views here.
 
+
 class ItemViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows Item to be viewed or edited.
-    """
+    # """
+    # API endpoint that allows Item to be viewed or edited.
+    # """
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly]
-                          
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-    # def create(self, *args, **kwargs):
-    #     super(Item, self).save(*args, **kwargs)
+        # def create(self, *args, **kwargs):
+        #     super(Item, self).save(*args, **kwargs)
+
 
 class ItemLinkViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows ItemLink to be viewed or edited.
-    """
+    # """
+    # API endpoint that allows ItemLink to be viewed or edited.
+    # """
     queryset = ItemLink.objects.all()
     serializer_class = ItemLinkSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -33,5 +37,21 @@ class ItemLinkViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        return Response({'status': 'success', 'pk': serializer.instance.pk})
+        return Response({"status": "success", "pk": serializer.instance.pk})
 
+
+class FriendViewSet(viewsets.ModelViewSet):
+    # queryset = Friend.objects.all()
+    queryset = Friend.objects.with_overdue()
+    serializer_class = FriendSerializer
+    permission_classes = [IsOwner]
+
+
+class BelongingViewSet(viewsets.ModelViewSet):
+    queryset = Belonging.objects.all()
+    serializer_class = BelongingSerializer
+
+
+class BorrowedViewSet(viewsets.ModelViewSet):
+    queryset = Borrowed.objects.all()
+    serializer_class = BorrowedSerializer
