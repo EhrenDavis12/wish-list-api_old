6945cd8bcd04b9bfac3a497f56d69134d2744ddc
Ehren Davis <ehren.davis12@gmail.com>
2021-08-28 20:22:56 -0700

    app group, fixturs, and testing

--- wish_list_api/settings.py
+++ wish_list_api/settings.py
@@ -165,4 +165,5 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated"),
+    # 'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
